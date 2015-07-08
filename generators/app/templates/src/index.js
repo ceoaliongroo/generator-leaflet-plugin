18ef2eead007708fc3d9e108691197232a9a73cd
Carlos Mantilla <cmantilla@aliongroo.com>
2015-07-08 19:08:18 +0300

    feat(generator): basic skeleton leaflet plugin

---
+++ generators/app/templates/src/index.js
@@ -0,0 +1,7 @@
+(function(){
+  // Add your Plugin implementation here.
+  // More information:
+  //  * How to create a Leaflet Plugin - https://github.com/Leaflet/Leaflet/blob/master/PLUGIN-GUIDE.md
+  //  * API Reference - http://leafletjs.com/reference.html
+
+})();
